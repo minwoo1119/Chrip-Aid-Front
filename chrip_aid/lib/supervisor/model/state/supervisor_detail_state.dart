a293ae6d95954f6397db7c9fe9b02943a67bbc08
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-09-18 13:48:09 +0900

    ✨ Add : 관리자 detail state 추가

---
+++ chrip_aid/lib/supervisor/model/state/supervisor_detail_state.dart
@@ -0,0 +1,13 @@
+import 'package:chrip_aid/common/value_state/state/value_state.dart';
+import 'package:chrip_aid/orphanage/model/entity/donate_entity.dart';
+import 'package:chrip_aid/orphanage/model/entity/orphanage_basket_entity.dart';
+import 'package:chrip_aid/orphanage/model/entity/orphanage_detail_entity.dart';
+import 'package:chrip_aid/orphanage/model/entity/orphanage_entity.dart';
+
+class OrphanageListState extends ValueStateNotifier<List<OrphanageEntity>> {}
+
+class OrphanageDetailState extends ValueStateNotifier<OrphanageDetailEntity> {}
+
+class OrphanageBasketState extends ValueStateNotifier<List<OrphanageBasketEntity>> {}
+
+class OrphanageDonateState extends ValueStateNotifier<List<DonateEntity>> {}
