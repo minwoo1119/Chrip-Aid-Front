8adb93cf42c653bc96553c7b223fbf940a2a15b1
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-09-21 20:03:37 +0900

    💄 Feat : 유저 상세 페이지 디자인 수정

--- chrip_aid/lib/common/component/custom_detail_info.dart
+++ chrip_aid/lib/common/component/custom_detail_info.dart
@@ -1,10 +1,10 @@
-import 'package:chrip_aid/orphanage/layout/detail_page_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:chrip_aid/orphanage/layout/detail_page_layout.dart';
 import '../../supervisor/viewmodel/supervisor_accountmanagement_viewmodel.dart';
 import '../styles/colors.dart';
 import '../styles/sizes.dart';
+import 'custom_user_list.dart';
 
 class CustomDetailInfo extends ConsumerWidget {
   final String name;
@@ -26,7 +26,6 @@ class CustomDetailInfo extends ConsumerWidget {
     required this.region,
   });
 
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final viewModel = ref.read(supervisorAccountManagementViewModelProvider)..getInfo();
@@ -34,6 +33,7 @@ class CustomDetailInfo extends ConsumerWidget {
     return DetailPageLayout(
       extendBodyBehindAppBar: false,
       title: '계정 관리',
+      titleColor: Colors.white,
       appBarBackgroundColor: CustomColor.buttonMainColor,
       backgroundColor: CustomColor.backgroundMainColor,
       leadingColor: CustomColor.textReverseColor,
@@ -48,60 +48,229 @@ class CustomDetailInfo extends ConsumerWidget {
         ),
         const SizedBox(width: kPaddingMiddleSize),
       ],
-      child: Container(
-        child: Column(
-          children: [
-             Column(
-               mainAxisAlignment: MainAxisAlignment.center,
-               children: [
-                 Row(
-                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                   children: [
-                     Image(
-                       image: AssetImage('./assets/image/logo.png'),
-                       height: 100,
-                       width: 100,
-                     ),
-                     Column(
-                     crossAxisAlignment: CrossAxisAlignment.start,
-                       children: [
-                         Text('이름  ${name}'),
-                         Text('아이디 ${email}'),
-                         Text('별명 ${nickname}'),
-                       ],
-                     )
-                   ],
-                 )
-               ],
-             ),
-             Column(
-               children: [
-                 Container(
-                   margin: EdgeInsets.all(30.0),
-                   child: Row(
-                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                     children: [
-                       Text('나이 ${age}'),
-                       SizedBox(width: 100,),
-                       Text('성별 ${sex}'),
-                     ],
-                   ),
-                 )
-               ],
-             ),
-             Column(
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-                 Text('전화번호 ${phoneNumber}'),
-                 Text('지역 ${region}'),
-               ],
-             ),
-             Column(
-               children: [
-                 Text('활동 내역'),
-               ],
-             ),
-          ],
+      child: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(20.0),
+          child: Container(
+            padding: EdgeInsets.all(30.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(
+                color: Colors.green,
+                width: 2.0,
+              ),
+            ),
+            child: Column(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/image/logo.png'),
+                          height: 100,
+                          width: 100,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  '이름',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                                SizedBox(width: 10.0,),
+                                Text('${name}'),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  '아이디',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                                SizedBox(width: 10.0,),
+                                Text('${email}'),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  '별명',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                                SizedBox(width: 10.0,),
+                                Text('${nickname}'),
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(height: 30,),
+                Column(
+                  children: [
+                    Container(
+                      child: Row(
+                        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 6.0),
+                            child: Row(
+                              children: [
+                                Text(
+                                  '나이',
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                SizedBox(width: 10.0,),
+                                Text('${age}'),
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 100,),
+                          Row(
+                            children: [
+                              Text(
+                                '성별',
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              SizedBox(width: 10.0,),
+                              Text('${sex}'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 6.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            '전화번호',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          SizedBox(width: 10.0,),
+                          Text('${phoneNumber}'),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 6.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            '지역',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          SizedBox(width: 10.0,),
+                          Text('${region}'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '활동 내역',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+
+                    Container(
+                      height: 300,
+                      child: ListView(
+                        children: [
+                          CustomUserList(
+                            name: '미누',
+                            email: 'email',
+                            phoneNumber: '010-0000-0000',
+                            nickname: 'nickname',
+                          ),
+                          CustomUserList(
+                            name: '미누',
+                            email: 'email',
+                            phoneNumber: '010-0000-0000',
+                            nickname: 'nickname',
+                          ),
+                          CustomUserList(
+                            name: '미누',
+                            email: 'email',
+                            phoneNumber: '010-0000-0000',
+                            nickname: 'nickname',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        print('ElevatedButton pressed');
+                      },
+                      child: Row(
+                        children: [
+                          Icon(Icons.edit),
+                          Text('수정'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 20.0,),
+                    ElevatedButton(
+                      onPressed: () {
+                        print('ElevatedButton pressed');
+                      },
+                      child: Row(
+                        children: [
+                          Icon(Icons.delete),
+                          Text('삭제'),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
