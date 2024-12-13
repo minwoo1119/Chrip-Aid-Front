a5bb5ba8f8381d00e8d605e8d9cc4d1d7faf8f7f
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-12-13 21:44:05 +0900

    🔥 ChatRoom 파일에서 불필요한 파일 분리
    - UserDto, OrphanageUserDto 분리
    - UserEntity, OrphanageUserEntity 분리
    - 기존에 존재하던 UserDto 및 UserEntity 사용하려 하였으나 필요한 필드가 달라 일단 새로 생성해둠

--- chrip_aid/lib/chatting/model/entity/chat_room_entity.dart
+++ chrip_aid/lib/chatting/model/entity/chat_room_entity.dart
@@ -1,3 +1,5 @@
+import 'package:chrip_aid/chatting/model/entity/chatting_orphanage_user_entity.dart';
+import 'package:chrip_aid/chatting/model/entity/chatting_user_entity.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'chat_room_entity.g.dart';
@@ -5,8 +7,8 @@ part 'chat_room_entity.g.dart';
 @JsonSerializable()
 class ChatRoomEntity {
   final String chatRoomId;
-  final UserEntity user; // 사용자 정보
-  final OrphanageUserEntity orphanageUser; // 보육원 사용자 정보
+  final ChattingUserEntity user; // 사용자 정보
+  final ChattingOrphanageUserEntity orphanageUser; // 보육원 사용자 정보
 
   ChatRoomEntity({
     required this.chatRoomId,
@@ -21,48 +23,4 @@ class ChatRoomEntity {
   Map<String, dynamic> toJson() => _$ChatRoomEntityToJson(this);
 }
 
-@JsonSerializable()
-class UserEntity {
-  final String userId;
-  final String name;
-  final String email;
-  final int age;
-  final String sex;
-  final String nickname;
-  final String region;
-  final String phoneNumber;
-  final String profilePhoto;
 
-  UserEntity({
-    required this.userId,
-    required this.name,
-    required this.email,
-    required this.age,
-    required this.sex,
-    required this.nickname,
-    required this.region,
-    required this.phoneNumber,
-    required this.profilePhoto,
-  });
-
-  factory UserEntity.fromJson(Map<String, dynamic> json) =>
-      _$UserEntityFromJson(json);
-  Map<String, dynamic> toJson() => _$UserEntityToJson(this);
-}
-
-@JsonSerializable()
-class OrphanageUserEntity {
-  final String orphanageUserId;
-  final String name;
-  final String email;
-
-  OrphanageUserEntity({
-    required this.orphanageUserId,
-    required this.name,
-    required this.email,
-  });
-
-  factory OrphanageUserEntity.fromJson(Map<String, dynamic> json) =>
-      _$OrphanageUserEntityFromJson(json);
-  Map<String, dynamic> toJson() => _$OrphanageUserEntityToJson(this);
-}
